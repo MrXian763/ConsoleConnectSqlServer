afb1f5bb853e98e0ebf9e5fde1c3c56d176b59f2
xianzicai <xianzhiyong763@gmail.com>
2024-10-07 13:07:06 +0800

    多语言支持

---
+++ ConsoleConnectSqlServer/InfoHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleConnectSqlServer
+{
+    class InfoHelper
+    {
+        public static string Sex1 { get; set; }
+        public static string Sex2 { get; set; }
+        public static string WelcomeInfo { get; set; }
+        public static string InputTipInfo { get; set; }
+        public static string InputUserNameInfo { get; set; }
+        public static string InputPasswordInfo { get; set; }
+        public static string WrongUserNameOrPasswordInfo { get; set; }
+        public static string WelcomeUseInfo { get; set; }
+        public static string InputSexInfo { get; set; }
+        public static string SexSelectInfo { get; set; }
+        public static string InputWrongInfo { get; set; }
+        public static string UpdateSuccessInfo { get; set; }
+        public static string UpdateErrInfo { get; set; }
+        public static string SelectAllUserInfo { get; set; }
+        public static string UserOutputInfo { get; set; }
+    }
+}
--- ConsoleConnectSqlServer/Program.cs
+++ ConsoleConnectSqlServer/Program.cs
@@ -8,17 +8,59 @@ namespace ConsoleConnectSqlServer
     {
         static void Main(string[] args)
         {
+
+            // 选择语言
+            Console.WriteLine("请选择系统语言：1-中文 2-English");
+            string inputLang = Console.ReadLine();
+
+            if (inputLang == "2")
+            {
+                InfoHelper.Sex1 = "Male";
+                InfoHelper.Sex2 = "Female"; 
+                InfoHelper.WelcomeInfo = "Welcome to the login system!!!";
+                InfoHelper.InputTipInfo = "Please enter your username and password below";
+                InfoHelper.InputUserNameInfo = "Please enter username:";
+                InfoHelper.InputPasswordInfo = "Please input a password:";
+                InfoHelper.WrongUserNameOrPasswordInfo = "Username or password error, please try again!";
+                InfoHelper.WelcomeUseInfo = "Welcome to use: @NickName";
+                InfoHelper.InputSexInfo = "Dear @NickName, please enter your gender information";
+                InfoHelper.SexSelectInfo = "Please select gender: 1- Male; 2- Female";
+                InfoHelper.InputWrongInfo = "Input error, please try again";
+                InfoHelper.UpdateSuccessInfo = "Update successful!";
+                InfoHelper.UpdateErrInfo = "Update failed!";
+                InfoHelper.SelectAllUserInfo = "Query all user information";
+                InfoHelper.UserOutputInfo = "Username: @UserName; Nickname: @NickName; Gender: @Sex";
+            }
+            else
+            {
+                InfoHelper.Sex1 = "男";
+                InfoHelper.Sex2 = "女";
+                InfoHelper.WelcomeInfo = "欢迎访问登录系统!!!";
+                InfoHelper.InputTipInfo = "请在下方输入您的用户名和密码";
+                InfoHelper.InputUserNameInfo = "请输入用户名：";
+                InfoHelper.InputPasswordInfo = "请输入密码：";
+                InfoHelper.WrongUserNameOrPasswordInfo = "用户名或密码错误，请重试!";
+                InfoHelper.WelcomeUseInfo = "欢迎使用：@NickName";
+                InfoHelper.InputSexInfo = "尊敬的@NickName，请录入性别信息";
+                InfoHelper.SexSelectInfo = "请选择性别：1-男；2-女";
+                InfoHelper.InputWrongInfo = "输入有误，请重试";
+                InfoHelper.UpdateSuccessInfo = "更新成功！";
+                InfoHelper.UpdateErrInfo = "更新失败！";
+                InfoHelper.SelectAllUserInfo = "查询所有用户信息";
+                InfoHelper.UserOutputInfo = "用户名：@UserName; 昵称：@NickName; 性别：@Sex";
+            }
+
             #region 登录
             // 打印系统基本信息
-            Console.WriteLine("欢迎访问登录系统!!!");
-            Console.WriteLine("请在下方输入您的用户名和密码");
+            Console.WriteLine(InfoHelper.WelcomeInfo);
+            Console.WriteLine(InfoHelper.InputTipInfo);
             DataTable dt;
             // 验证用户名和密码
             while (true)
             {
-                Console.Write("请输入用户名：");
+                Console.Write(InfoHelper.InputUserNameInfo);
                 string inputUserName = Console.ReadLine();
-                Console.Write("请输入密码：");
+                Console.Write(InfoHelper.InputPasswordInfo);
                 string inputPassword = Console.ReadLine();
 
                 // 将用户的输入与数据库中的数据比对
@@ -27,37 +69,35 @@ namespace ConsoleConnectSqlServer
 
                 int count = dt.Rows.Count;
                 if (count <= 0)
-                    Console.WriteLine("用户名或密码错误，请重试");
+                    Console.WriteLine(InfoHelper.WrongUserNameOrPasswordInfo);
                 else
                     break;
             }
-            Console.WriteLine($"欢迎使用：{dt.Rows[0]["NickName"]}");
+            string str = InfoHelper.WelcomeUseInfo.Replace("@NickName", dt.Rows[0]["NickName"].ToString());
+            Console.WriteLine(str);
             // 读取性别，如果性别为空强制用户录入
             string sex = dt.Rows[0]["Sex"].ToString();
             if (string.IsNullOrEmpty(sex))
             {
-                Console.WriteLine($"尊敬的{dt.Rows[0]["NickName"]}，请录入性别信息");
+                str = InfoHelper.InputSexInfo.Replace("@NickName", dt.Rows[0]["NickName"].ToString());
+                Console.WriteLine(str);
                 while (true)
                 {
-                    Console.WriteLine("请选择性别：1-男；2-女");
+                    Console.WriteLine(InfoHelper.SexSelectInfo);
                     string selectSex = Console.ReadLine();
-                    if (selectSex == "1")
-                        sex = "男";
-                    else if (selectSex == "2")
-                        sex = "女";
-                    else
+                    if (selectSex != "1" && selectSex != "2")
                     {
-                        Console.WriteLine("输入有误，请重试");
+                        Console.WriteLine(InfoHelper.InputWrongInfo);
                         continue;
                     }
 
                     // 更新性别到数据库
-                    string updateSql = $"update UserT set Sex = '{sex}' where UserName = '{dt.Rows[0]["UserName"]}';";
+                    string updateSql = $"update UserT set Sex = '{selectSex}' where UserName = '{dt.Rows[0]["UserName"]}';";
                     int count = EditData(updateSql);
                     if (count > 0)
-                        Console.WriteLine("更新成功！");
+                        Console.WriteLine(InfoHelper.UpdateSuccessInfo);
                     else
-                        Console.WriteLine("更新失败！");
+                        Console.WriteLine(InfoHelper.UpdateErrInfo);
                     break;
                 }
                 
@@ -66,10 +106,20 @@ namespace ConsoleConnectSqlServer
             // 查询展示所有用户列表   
             string selectAllUser = "select * from UserT;";
             dt = SelectData(selectAllUser);
-            Console.WriteLine("查询所有用户信息");
+            Console.WriteLine(InfoHelper.SelectAllUserInfo);
             for (int i = 0; i < dt.Rows.Count; i++)
             {
-                Console.WriteLine($"用户名：{dt.Rows[i]["UserName"]}; 昵称：{dt.Rows[i]["NickName"]}; 性别：{dt.Rows[i]["Sex"]}");
+                string sex1 = dt.Rows[i]["Sex"].ToString();
+                if (sex1 == "1")
+                    sex1 = InfoHelper.Sex1;
+                else if (sex1 == "2")
+                    sex1 = InfoHelper.Sex2;
+
+                str = InfoHelper.UserOutputInfo;
+                str = str.Replace("@NickName", dt.Rows[i]["NickName"].ToString());
+                str = str.Replace("@Sex", dt.Rows[i]["Sex"].ToString());
+                str = str.Replace("@UserName", dt.Rows[i]["UserName"].ToString());
+                Console.WriteLine(str);
             }
             #endregion
 
@@ -131,6 +181,9 @@ namespace ConsoleConnectSqlServer
             return table;
         }
 
+        /// <summary>
+        /// 增删改查演示
+        /// </summary>
         private static void ShowCRUD()
         {
             #region 新增
