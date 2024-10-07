6b74c3c0b772d68fb14981ce24db4ee49fc524b3
xianzicai <xianzhiyong763@gmail.com>
2024-10-07 10:28:42 +0800

    添加登录功能

--- ConsoleConnectSqlServer/Program.cs
+++ ConsoleConnectSqlServer/Program.cs
@@ -8,7 +8,106 @@ namespace ConsoleConnectSqlServer
     {
         static void Main(string[] args)
         {
+            #region 登录
+            // 打印系统基本信息
+            Console.WriteLine("欢迎访问登录系统!!!");
+            Console.WriteLine("请在下方输入您的用户名和密码");
+            Console.Write("请输入用户名：");
+            string inputUserName = Console.ReadLine();
+            Console.Write("请输入密码：");
+            string inputPassword = Console.ReadLine();
 
+            // 将用户的输入与数据库中的数据比对
+            string selectSql = $"select * from UserT where UserName = '{inputUserName}' and Password = '{inputPassword}'";
+            DataTable dt = SelectData(selectSql);
+
+            int count = dt.Rows.Count;
+            if (count <= 0)
+            {
+                Console.WriteLine("用户名或密码错误");
+                return;
+            }
+
+            Console.WriteLine($"欢迎使用：{dt.Rows[0]["NickName"]}");
+
+            //bool flag = false;
+            //for (int i = 0; i < dt.Rows.Count; i++)
+            //{
+            //    string userName = dt.Rows[i]["UserName"].ToString();
+            //    string password = dt.Rows[i]["Password"].ToString();
+
+            //    if (inputPassword == password && inputUserName == userName)
+            //    {
+            //        flag = true;
+            //        break;
+            //    }
+            //}
+            //if (flag)
+            //    Console.WriteLine("用户名与密码正确");
+            //else
+            //    Console.WriteLine("用户名或密码错误");
+            #endregion
+
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 执行修改语句
+        /// </summary>
+        /// <param name="sql">要执行的SQL语句</param>
+        /// <returns>受影响行数</returns>
+        private static int EditData(string sql)
+        {
+            // 连接数据库
+            SqlConnection connection = new SqlConnection();
+            // 连接信息
+            connection.ConnectionString = "Server=localhost;Database=TestDB;Trusted_Connection=true;";
+            connection.Open(); // 打开连接
+            SqlCommand command = new SqlCommand(sql, connection);
+            int count = 0 ;
+            try
+            {
+                count = command.ExecuteNonQuery(); // 执行新增语句，返回受影响行数
+            }
+            catch (Exception ex)
+            {
+                count = -1;
+                Console.WriteLine(ex);
+            }
+            connection.Close(); // 关闭连接
+            return count;
+        }
+
+        /// <summary>
+        /// 查询数据
+        /// </summary>
+        /// <param name="sql">要执行的sql语句</param>
+        /// <returns>查询出来的数据</returns>
+        private static DataTable SelectData(string sql)
+        {
+            // 连接数据库
+            SqlConnection connection = new SqlConnection();
+            // 连接信息
+            connection.ConnectionString = "Server=localhost;Database=TestDB;Trusted_Connection=true;";
+            connection.Open(); // 打开连接
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = sql; // 查询语句
+
+            // 提交sql语句
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+
+            // 得到返回结果
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            DataTable table = ds.Tables[0];
+            return table;
+        }
+
+        private static void ShowCRUD()
+        {
             #region 新增
             string userName = "1111";
             string password = "222";
@@ -51,61 +150,6 @@ namespace ConsoleConnectSqlServer
                 Console.WriteLine($"{table.Rows[i]["UserName"]}  {table.Rows[i]["Password"]}  {table.Rows[i]["NickName"]}");
             }
             #endregion
-
-            Console.ReadKey();
-        }
-
-        /// <summary>
-        /// 执行修改语句
-        /// </summary>
-        /// <param name="sql">要执行的SQL语句</param>
-        /// <returns>受影响行数</returns>
-        private static int EditData(string sql)
-        {
-            // 连接数据库
-            SqlConnection connection = new SqlConnection();
-            // 连接信息
-            connection.ConnectionString = "Server=localhost;Database=TestDB;Trusted_Connection=true;";
-            connection.Open(); // 打开连接
-            SqlCommand command = new SqlCommand(sql, connection);
-            int count = 0 ;
-            try
-            {
-                count = command.ExecuteNonQuery(); // 执行新增语句，返回受影响行数
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-            connection.Close(); // 关闭连接
-            return count;
-        }
-
-        /// <summary>
-        /// 查询数据
-        /// </summary>
-        /// <param name="sql">要执行的sql语句</param>
-        /// <returns>查询出来的数据</returns>
-        private static DataTable SelectData(string sql)
-        {
-            // 连接数据库
-            SqlConnection connection = new SqlConnection();
-            // 连接信息
-            connection.ConnectionString = "Server=localhost;Database=TestDB;Trusted_Connection=true;";
-            connection.Open(); // 打开连接
-            SqlCommand command = new SqlCommand();
-            command.Connection = connection;
-            command.CommandText = sql; // 查询语句
-
-            // 提交sql语句
-            SqlDataAdapter adapter = new SqlDataAdapter();
-            adapter.SelectCommand = command;
-
-            // 得到返回结果
-            DataSet ds = new DataSet();
-            adapter.Fill(ds);
-            DataTable table = ds.Tables[0];
-            return table;
         }
     }
 }
